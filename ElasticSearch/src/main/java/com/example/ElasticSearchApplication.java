7eb8a40706de3ef414aee2847aceac2ca0fdedfc
dangvanhien98 <nhinchi1111@gmail.com>
2023-03-13 17:34:40 +0700

    first commit

---
+++ ElasticSearch/src/main/java/com/example/ElasticSearchApplication.java
@@ -0,0 +1,17 @@
+ package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableElasticsearchRepositories(basePackages  = "com.example.EsRepository")
+@EnableJpaRepositories(basePackages = "com.example.Repository")
+public class ElasticSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchApplication.class, args);
+	}
+
+}
